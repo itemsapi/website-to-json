4a6319045a1a6318b545d4bec7d9949bebe22ce3
Mateusz Rzepa <mrzepa89@gmail.com>
2016-12-03 11:27:33 +0100

    phantomjs

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var _ = require('lodash');
 var Promise = require('bluebird');
 var request = Promise.promisifyAll(require('request'));
 var converter = require('./src/html-to-data')
+//var phantomjs = require('phantomjs-bin');
+var Horseman = require('node-horseman');
 
-exports.processUrlWithRequestAsync = function(data, options) {
+exports.processUrlWithRequestAsync = function(url, options) {
+  options = options || {}
   return request.getAsync({
-    url: data.url,
+    url: url,
     jar: true,
     gzip: true,
     timeout: options.timeout || 3000,
@@ -24,6 +27,39 @@ exports.processUrlWithRequestAsync = function(data, options) {
   })
 }
 
+exports.processUrlWithPhantomAsync = function(url, options) {
+  options = options || {}
+  return new Promise(function(resolve, reject) {
+    var horseman = new Horseman({
+      loadImages: false,
+      timeout: options.timeout || 5000,
+      injectJquery: true,
+      //proxy: data.proxy,
+      //phantomPath: phantomjs.path
+    });
+    horseman
+    //.userAgent(data.userAgent)
+    .open(url)
+    /*.on('error', function(msg) {
+      throw new Error('Unexpected error')
+    })*/
+    .html()
+    .then(function(body) {
+      return resolve(body)
+    })
+  })
+  .catch((err) => {
+    throw new Error('Unexpected error')
+  })
+}
+
+exports.processUrlAsync = function(url, data) {
+  if (data.type === 'phantomjs') {
+    return exports.processUrlWithPhantomAsync(url, data);
+  }
+  return exports.processUrlWithRequestAsync(url, data);
+}
+
 /**
  * recipes
  * timeout
@@ -38,9 +74,7 @@ exports.extractUrl = function(url, options) {
     url = 'http://' + url
   }
 
-  return exports.processUrlWithRequestAsync({
-    url: url
-  }, options)
+  return exports.processUrlAsync(url, options)
   .catch((err) => {
     throw new Error('Url ' + url + ' seems to be not valid ' + err)
   })
